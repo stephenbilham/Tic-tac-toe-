bf0c1d373117c4aadc7933bd30083852230cb080
stephenbilham <stephenbilham@yahoo.com>
2019-11-11 16:14:13 -0800

    updated game

--- src/Board.js
+++ src/Board.js
@@ -1,122 +1,110 @@
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
-import {Box} from './Box.js'
-import Functions from './Functions'
-import {findWinner} from './Functions.js'
-import {allBoxesClicked} from './Functions.js'
-import {changePlayer} from './Functions.js'
+import Box from './Box.js';
+import Functions from './Functions';
+import {findWinner} from './Functions.js';
+import {allBoxesClicked} from './Functions.js';
+import {changePlayer} from './Functions.js';
+import winningConditions from './winningConditions.js'
+
+let boxes = Array(9).fill(0);
+let player1 = [];
+let player2 = [];
+let winningPlayer = 0
+
+
 
 class Board extends Component{
     constructor (props) {
         super(props)
 
         this.state = {
-            xTurn: true,
-            history: [],
-            boxes: Array(9).fill(null),
+            player1Turn: true,
+            gameOver: false
         }
     }
 
+    handleClick = (index) => {
+        if(boxes[index] != 0){
+            return
+        }
+        let {player1Turn,gameOver} = this.state
+        if(gameOver){
+            return
+        }
+        boxes[index] = player1Turn ? 1 : 2
 
-    handleOnClick = (index) => {
-        let { history, boxes, noTurn, yesTurn } = this.state
-        boxes.value = index
+        if(player1Turn) {
+            player1.push(index)   
+        } else {
+            player2.push(index)
+        }
 
-        if (boxes[index] == null) {
-            if (findWinner(boxes)){
-                return
-            }
-            if (allBoxesClicked(boxes) === true) {
-                return
-            }
-
-
-            boxes[index] = this.state.xTurn ? '❌': '⭕️'
-            history.push(this.state.xTurn ? '❌': '⭕️')
-
-            this.setState({
-                boxes: boxes,
-                history: history,
-                xTurn: !this.state.xTurn,
-            })
+        let curPlayerWon = this.findIfWinner()
+        if(curPlayerWon){
+            winningPlayer = player1Turn ? 1 : 2
+            this.setState({gameOver:true})
+        } else {
+            this.setState({player1Turn: !player1Turn})
         }
     }
 
+    findIfWinner = () => {
+        let {player1Turn} = this.state
+        let playerBoxes = player1Turn ? player1 : player2
+        let playerWon = true
+        for(let i = 0; i < winningConditions.length; i++){
+            let winningCondition = winningConditions[i] //e.g [0,1,2]
+            playerWon = true
+            for(let j = 0; j < 3; j++){
+                let curBox = winningCondition[j] //[0,1,2] curBox => number 
+                if(!playerBoxes.includes(curBox)){
+                    playerWon = false
+                    break
+                }
+            }
+            if(playerWon){
+                break
+            }
+        }
+        return playerWon
+    }
+
+    getTitleText = () => { 
+        let {player1Turn,gameOver} = this.state
+        if(gameOver){
+            return winningPlayer === 1 ? "Rick Wins!" : "Morty's wins!"
+        }
+        else {
+            return player1Turn ? `It's your turn Rick` : `It's your Morty`
+        }
+    }
 
     handleReset = () => {
-        let { history, boxes } = this.state
-
-        this.setState(
-            {boxes: Array(9).fill(null),
-            history: [],}
-        )
+        boxes = Array(9).fill(0)
+        player1 = [];
+        player2 = [];
+        winningPlayer = 0
+        this.setState({player1Turn:true, gameOver: false})
     }
 
-
     render() {
-
-    const winner = findWinner(this.state.boxes)
-    const isFilled = allBoxesClicked(this.state.boxes)
-
-    let status
-
-        if (winner) {
-            status = `The winner is ${winner}!`
-        } else if(!winner && isFilled) {
-            status = 'Game drawn!'
-        } else {
-            status = `It is your turn ${(this.state.xTurn ? 'player one ❌' : 'player two ⭕️')}.`
-        }
-
+        let grid = boxes.map((box, i ) => {
+            return (
+                <Box key={i.toString()} id={i} value={box} handleClick={this.handleClick}/>
+            )
+        })
         return (
-          <div className="App">
-              <h2>{status}</h2>
-                <div className= "boardSize" >
-                    <Box
-                    onClick = { () => this.handleOnClick(0)}
-                    value = {this.state.boxes[0]}
-                    />
-                    <Box
-                    onClick = { () => this.handleOnClick(1)}
-                    value = {this.state.boxes[1]}
-                    />
-                    <Box
-                    onClick = { () => this.handleOnClick(2)}
-                    value = {this.state.boxes[2]}
-                    />
-                    <br />
-                    <Box
-                    onClick = { () => this.handleOnClick(3)}
-                    value = {this.state.boxes[3]}
-                    />
-                    <Box
-                    onClick = { () => this.handleOnClick(4)}
-                    value = {this.state.boxes[4]}
-                    />
-                    <Box
-                    onClick = { () => this.handleOnClick(5)}
-                    value = {this.state.boxes[5]}
-                    />
-                    <br />
-                    <Box
-                    onClick = { () => this.handleOnClick(6)}
-                    value = {this.state.boxes[6]}
-                    />
-                    <Box
-                    onClick = { () => this.handleOnClick(7)}
-                    value = {this.state.boxes[7]}
-                    />
-                    <Box
-                    onClick = { () => this.handleOnClick(8)}
-                    value = {this.state.boxes[8]}
-                    />
-                    <br />
-                </div>
+            <div>
+                <h2>{this.getTitleText()}</h2>
+                    <div className= "grid" >
+                        {grid}
+                    </div>
                 <button id = 'resetButton' onClick = {this.handleReset}> Restart Game </button>
-          </div>
+            </div>
         );
-      }
+    }
 };
 
 
--- src/Box.js
+++ src/Box.js
@@ -1,10 +1,23 @@
 
 import React, { Component } from 'react'
+import rick from "./rick.png"
+import morty from "./morty.png"
+import blank from "./blank.png"
 
-export const Box = (props) => {
-    return (
-            <button onClick = {props.onClick} className = "buttonOne" style = {{padding: "80px"}}>
-            {props.value}
-            </button>
-    )
+class Box extends Component {
+
+    handleClick = () => {
+        const{id, handleClick} = this.props
+        return handleClick(id)
+    }
+
+    render() {
+        return (
+            <div onClick = {this.handleClick} className = "box">
+                <img className = "face" src ={this.props.value === 0 ? blank: this.props.value === 1 ? rick : morty } alt = {blank}></img>
+            </div>
+        );
+    }
 }
+
+export default Box;
